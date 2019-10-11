4979b9ac64731c63cfca496f4f3105de5cba8daa
adam <adamsiedlec@wp.pl>
2019-10-11 11:04:38 +0200

    I have fixed logo for non-logged users.

--- src/main/java/pl/adamsiedlecki/spring/MainView.java
+++ src/main/java/pl/adamsiedlecki/spring/MainView.java
@@ -1,18 +1,17 @@
 package pl.adamsiedlecki.spring;
 
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.server.VaadinServletService;
+import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import pl.adamsiedlecki.spring.tool.ResourceGetter;
 
 @Route("")
 @PWA(name = "Project for secure communication", shortName = "ComCrypt")
@@ -23,10 +22,8 @@ public class MainView extends VerticalLayout {
     @Autowired
     public MainView(Environment env) {
         Notification.show("Welcome to the S. Technologies Communication System").setPosition(Notification.Position.BOTTOM_CENTER);
-        Image appNameLabel = ResourceGetter.getComCryptLogo();
-        //appNameLabel.setId("app-name");
-        appNameLabel.setVisible(true);
-        add(appNameLabel);
+        Image img2 = new Image("images/comcryptLogo.png","Vaadin Logo");
+        add(img2);
         setAlignItems(Alignment.CENTER);
     }
 
